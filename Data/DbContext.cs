f46fdb6b355f7d1ba0f2ae3c30e698b966993a9f
Nuriddin Asrorov <denrov13@gmail.com>
2024-02-09 08:30:03 +0500

    Add project files.

---
+++ Data/DbContext.cs
@@ -0,0 +1,23 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VSMongoDB.Data
+{
+    public class DbContext
+    {
+        private readonly MongoClient mongoClient;
+        public DbContext()
+        {
+            this.mongoClient = new MongoClient("mongodb://localhost:27017");
+
+            foreach (var db in mongoClient.ListDatabases().ToList())
+            {
+                Console.WriteLine(db);
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using VSMongoDB.Data;
+
+namespace VSMongoDB
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var dbContext = new DbContext();
+        }
+    }
+}
